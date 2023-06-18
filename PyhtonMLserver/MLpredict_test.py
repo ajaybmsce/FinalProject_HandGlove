c2922ef9b169122ffc9f1bf716fdf666019ecddb
ajaybmsce <ajaypatel.te19@bmsce.ac.in>
2023-06-18 17:03:41 +0530

    commit

--- PyhtonMLserver/MLpredict_test.py
+++ PyhtonMLserver/MLpredict_test.py
@@ -5,33 +5,37 @@ import numpy as np
 
 UDP_IP = "0.0.0.0"
 UDP_PORT = 8080
-streamlit_ip = "192.168.137.23"
-streamlit_port = "8888"
+
 # Load the trained model
-model = tf.keras.models.load_model('C:/Users/ajayp/OneDrive/Documents/PlatformIO/FinalProject_HandGlove/PyhtonMLserver/trained_model.h5')
+model = tf.keras.models.load_model('trained_model.h5')
 
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as UDPClientSocket:
-    UDPClientSocket.bind((UDP_IP, UDP_PORT))
-    print("Running...")
+# Create a UDP socket
+UDPClientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+UDPClientSocket.bind((UDP_IP, UDP_PORT))
+print("ML Server running...")
 
-    while True:
-        data, addr = UDPClientSocket.recvfrom(2048)
-        json_data = json.loads(data.decode('utf-8'))
+while True:
+    data, addr = UDPClientSocket.recvfrom(2048)
+    json_data = json.loads(data.decode('utf-8'))
 
-        # Extract the sensor values from the received JSON data
-        sensor_values = []
-        for i in range(6):
-            sensor_values.extend(list(json_data[str(i)].values()))
+    # Extract the sensor values from the received JSON data
+    sensor_values = []
+    for i in range(6):
+        sensor_values.extend(list(json_data[str(i)].values()))
 
-        # Convert the sensor values to a numpy array
-        input_data = np.array(sensor_values).reshape(1, 36)
+    # Convert the sensor values to a numpy array
+    input_data = np.array(sensor_values).reshape(1, 36)
 
-        # Perform prediction using the loaded model
-        prediction = model.predict(input_data)
-        predicted_class = np.argmax(prediction)
+    # Perform prediction using the loaded model
+    prediction = model.predict(input_data)
+    predicted_class = np.argmax(prediction)
 
-        print("Predicted class:", predicted_class)
+    print("Predicted class:", predicted_class)
 
-        # Send predicted valye to streamlit app
-        message = str(predicted_class).encode('utf-8')
-        UDPClientSocket.sendto(message,(streamlit_ip,streamlit_port))
+    # Send predicted value to Streamlit app using TCP
+    streamlit_ip = "127.0.0.1"
+    streamlit_port = 9000
+    TCPClientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    TCPClientSocket.connect((streamlit_ip, streamlit_port))
+    TCPClientSocket.send(str(predicted_class).encode('utf-8'))
+    TCPClientSocket.close()
